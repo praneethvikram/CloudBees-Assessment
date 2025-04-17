c625f9437ad33c3249fa665e7ef1532be1eefbfa
praneethvikram <praneethvikram9699@gmail.com>
2025-04-18 00:01:18 +0530

    First Commit

---
+++ src/main/java/com/cloudbees/CloudbeesApplication.java
@@ -0,0 +1,13 @@
+package com.cloudbees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudbeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudbees/CloudbeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudbees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
